13c5ad079c175daa76319a0ecc531a8896c74ea4
siddharthbhurat4 <sbhurat@umich.edu>
2023-12-01 20:06:27 +0000

    Updated the main running script run_demo to source the bashrc and also get the executables

--- run_demo.sh
+++ run_demo.sh
@@ -3,11 +3,28 @@
 grep -qxF "$export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:/workspaces/ros_ws/src/basic_mobile_robot/models/" ~/.bashrc || echo "export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:/workspaces/ros_ws/src/basic_mobile_robot/models/" >> ~/.bashrc 
 source ~/.bashrc
 
-# Script to build, setup, and launch ROS2 package
-cd src
 # Run colcon build
 colcon build
+ 
+# Get the current directory
+current_directory=$(pwd)
+
+# Source directories
+controller_source="/workspaces/ros_ws/install/mobile_robot_localization/lib/mobile_robot_localization/controller_node"
+localization_source="/workspaces/ros_ws/install/mobile_robot_localization/lib/mobile_robot_localization/localization_node"
+
+# Destination directory
+destination_directory="/workspaces/ros_ws/src/install/mobile_robot_localization/lib/mobile_robot_localization"
+
+
+# Copy files
+cp "$controller_source" "$destination_directory"
+cp "$localization_source" "$destination_directory"
+
+# # Script to build, setup, and launch ROS2 package
+
 # Source the setup.bash file
 source install/setup.bash
+source ~/.bashrc
 # Launch the ROS2 package
 ros2 launch final_demo final_demo.launch.py
