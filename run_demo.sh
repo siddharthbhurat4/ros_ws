cae193225d917dc90d64259cf43ec4bb64a2820d
siddharthbhurat4 <sbhurat@umich.edu>
2023-12-01 13:51:07 +0000

    updated run script

--- run_demo.sh
+++ run_demo.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
+grep -qxF "$export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:/workspaces/ros_ws/src/basic_mobile_robot/models/" ~/.bashrc || echo "export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:/workspaces/ros_ws/src/basic_mobile_robot/models/" >> ~/.bashrc 
+source ~/.bashrc
+
 # Script to build, setup, and launch ROS2 package
 cd src
-
 # Run colcon build
 colcon build
-
 # Source the setup.bash file
 source install/setup.bash
-
 # Launch the ROS2 package
 ros2 launch final_demo final_demo.launch.py
